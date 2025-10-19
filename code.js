c87ab46a2c97b02925a737bcc517ca5276950fee
dev-AlexSv <janalexandersilvestree@gmail.com>
2025-10-19 17:57:37 +0800

    final rps-ui changes
    By devAlex

--- code.js
+++ code.js
@@ -16,39 +16,38 @@ function getPlayerChoice() {
     return choice;
 }
 
-function playRound() {
-    let playerChoice = getPlayerChoice();
+function playRound(selection) {
     let computerChoice = getComputerChoice();
 
-    if (playerChoice.toLowerCase === "rock") {
+    if (selection.toLowerCase === "rock") {
         if (computerChoice === "paper") {
-            alert("Computer wins this round...");
+            results.textContent = "Computer wins this round";
             computerScore++;
         } else if (computerChoice === "scissors") {
-            alert("Player wins this round...");
+            results.textContent = "Player wins this round";
             playerScore++;
         } else {
-            alert("No winner this round...");
+            results.textContent =  "No winner this round";
         }
-    } else if (playerChoice.toLowerCase === "scissors") {
+    } else if (selection.toLowerCase === "scissors") {
         if (computerChoice === "rock") {
-            alert("Computer wins this round...");
+            results.textContent =  "Computer wins this round";
             computerScore++;
         } else if (computerChoice === "paper") {
-            alert("Player wins this round...");
+            results.textContent =  "Player wins this round";
             playerScore++;
         } else {
-            alert("No winner this round...");
+            results.textContent = "No winner this round";
         }
     } else {
         if (computerChoice === "scissors") {
-            alert("Computer wins this round...");
+            results.textContent =  "Computer wins this round";
             computerScore++;
         } else if (computerChoice === "rock") {
-            alert("Player wins this round...");
+            results.textContent =  "Player wins this round";
             playerScore++;
         } else {
-            alert("No winner this round...");
+            results.textContent = "No winner this round";
         }
     }
     console.log(playerScore, computerScore);
@@ -64,38 +63,65 @@ function playRound() {
 
 
 function showWinner(computerScore, playerScore) {
-    if (playerScore > computerScore) {
-        alert("Player has won the game");
-    } else if (playerScore < computerScore) {
-        alert("Computer has won the game");
-    } else {
-        alert("Computer and player is tied. No winner")
+    if (playerScore === 5 || computerScore === 5) {
+        if (playerScore === 5) {
+            results.textContent = "Player has won the game";
+             results.style["background-color"] = "green";
+        } else {
+            results.textContent = "Computer has won the game";
+            results.style["background-color"] = "red";
+        }
+
+        rockButton.style["display"] = "none";
+        paperButton.style["display"] = "none";
+        scissorsButton.style["display"] = "none";
+        playAgainButton.style["visibility"] = "visible";
     }
 }
 
+function resetGame() {
+    computerScore = 0;
+    playerScore = 0;
+
+    results.textContent = "";
+    rockButton.style["display"] = "";
+    paperButton.style["display"] = "";
+    scissorsButton.style["display"] = "";
+    playAgainButton.style["visibility"] = "hidden";
+}
 let computerScore = 0;
 let playerScore = 0;
 
-
-let buttonElement = document.getElementById("start-button");
-console.log(buttonElement);
 
 // Event Listeners per button
 const rockButton = document.querySelector("#rock");
 const paperButton = document.querySelector("#paper");
 const scissorsButton = document.querySelector("#scissors");
+const results = document.querySelector(".results");
+
+const playAgainButton = document.querySelector(".bt-reset");
+
+
 
 rockButton.addEventListener("click", () => {
-
+    let playerChoice = "rock";
+    playRound(playerChoice);
+    showWinner(computerScore, playerScore);
 });
 
 paperButton.addEventListener("click", () => {
-
+    let playerChoice = "paper";
+    playRound(playerChoice);
+    showWinner(computerScore, playerScore);
 });
 
 scissorsButton.addEventListener("click", () => {
-
+    let playerChoice = "scissors";
+    playRound(playerChoice);
+    showWinner(computerScore, playerScore);
 });
 
+
+playAgainButton.addEventListener("click", resetGame);
 
 
